039194061ba2f08c285a8ebf12982ffb5334d6f2
mdeejay <mdjrussia@gmail.com>
2012-10-04 06:17:16 -0700

    Revert "endeavoru: use init and ueventd from CM main stream"
    This reverts commit eab497e655f5a840e7d59fd154e4a56cfce3abed

--- full_endeavoru.mk
+++ full_endeavoru.mk
@@ -24,8 +24,9 @@ PRODUCT_COPY_FILES := \
 	device/htc/endeavoru/ramdisk/init.rc:root/init.rc \
 	device/htc/endeavoru/ramdisk/init.endeavoru.rc:root/init.endeavoru.rc \
 	device/htc/endeavoru/ramdisk/init.usb.rc:root/init.usb.rc \
+	device/htc/endeavoru/ramdisk/ueventd.rc:root/ueventd.rc \
 	device/htc/endeavoru/ramdisk/ueventd.endeavoru.rc:root/ueventd.endeavoru.rc \
-	device/htc/endeavoru/ramdisk/fstab.endeavoru:root/fstab.endeavoru
+	device/htc/endeavoru/ramdisk/init:root/init
 
 # add post-boot which moves hardcoded stuff to user side from now on
 PRODUCT_COPY_FILES += \
